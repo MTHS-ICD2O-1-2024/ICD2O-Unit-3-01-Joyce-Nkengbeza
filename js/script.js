b2c27c24d12f0d718dcefc867866b1b5572e3fa4
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-26 17:15:50 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Joyce Nkengbeza
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculatesalary() {
+  // input
+  const hoursworked = parseFloat(document.getElementById('hours-worked').value);
+  const hourlywage = parseFloat(document.getElementById('hourly-wage').value);
+    
+
+  // process
+    const salaryweekly = (hoursworked * hourlywage) * (1.00 - 0.18)
+    const incometax = (hoursworked * hourlywage) * 0.18
+
+  // output
+  document.getElementById('salary').innerHTML = 'Your pay will be: $' +  salaryweekly.toFixed(2)
+  document.getElementById('incometax').innerHTML = 'The Government will take: $' + incometax.toFixed(2)
+}
+
